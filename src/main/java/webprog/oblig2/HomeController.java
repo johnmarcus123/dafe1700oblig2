e43ea561f12d76008cbc4724d496dd78efe4f8b2
johnmarcus123 <jodav3949@oslomet.no>
2024-03-16 18:13:16 +0100

    Lagde springboot.java, og bestilleskjema som tar variablene med getter og setter

--- src/main/java/webprog/oblig2/HomeController.java
+++
@@ -1,9 +0,0 @@
-package webprog.oblig2;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HomeController {
-    @GetMapping("/")
-}
---
+++ src/main/java/webprog/oblig2/SpringBoot.java
@@ -0,0 +1,12 @@
+package webprog.oblig2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBoot{
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot.class, args);
+    }
+}
