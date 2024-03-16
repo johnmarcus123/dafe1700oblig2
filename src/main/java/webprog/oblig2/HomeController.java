4ef6c9296b0650817ef457ec8a0d777fda2e3952
johnmarcus123 <jodav3949@oslomet.no>
2024-03-16 17:08:59 +0100

    lagd read.me

---
+++ src/main/java/webprog/oblig2/HomeController.java
@@ -0,0 +1,9 @@
+package webprog.oblig2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+}
